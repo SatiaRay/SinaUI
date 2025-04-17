36bdf2e81f41b3df08164d1247dc27770c267fbd
Mahdi <mahmodimahdi26@gmail.com>
2025-04-17 03:09:39 -0700

    chore: import axios instance from util/axios.js in Loign.js and take REACT_APP_API_URL as base url of axios object.

--- src/utils/axios.js
+++ src/utils/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 // Set the base URL for all API requests
-axios.defaults.baseURL = 'http://localhost:8000';
+axios.defaults.baseURL = process.env.REACT_APP_API_URL || 'http://localhost:8000';
 
 // Add a request interceptor to include the auth token
 axios.interceptors.request.use(
