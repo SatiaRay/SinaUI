21c0872bcd8b8ba445166a5009e7521262c441a2
Alireza Miladi <alireza.miladi1997@gmail.com>
2025-07-27 11:26:31 +0330

    refactor: remove npm menu package
    * fix answer message bug
    
    * clear codes
    
    * fix userInfo folder

--- src/components/Chat/Chat.js
+++ src/components/Chat/Chat.js
@@ -1015,7 +1015,7 @@ const Chat = () => {
   return (
     <>
       <style>{globalStyles}</style>
-      <div className="flex flex-col h-full p-6 max-w-7xl mx-auto">
+      <div className="flex flex-col h-screen p-6 max-w-7xl mx-auto">
         <div
           ref={chatContainerRef}
           className="flex-1 overflow-y-auto mb-4 space-y-4"
