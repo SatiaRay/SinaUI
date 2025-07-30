390f90f07d62d5a1e1eaf89da1c812669c226292
a-miladi <alireza.miladi1997@gmail.com>
2025-07-30 10:05:39 +0330

    set new icon and using toast for success copied

--- src/components/Chat/Chat.js
+++ src/components/Chat/Chat.js
@@ -5,6 +5,7 @@ import { askQuestion } from "../../services/api";
 import { getWebSocketUrl } from "../../utils/websocket";
 import VoiceBtn from "./VoiceBtn";
 import { WizardButtons } from "./Wizard/";
+import { toast } from "react-toastify";
 
 // استایل‌های سراسری برای پیام‌های چت
 const globalStyles = `
@@ -1002,7 +1003,12 @@ const Chat = ({ item }) => {
       .writeText(plainText)
       .then(() => {
         setCopiedMessageId(messageIndex);
-        setTimeout(() => setCopiedMessageId(null), 4000); // Reset after 2 seconds
+        toast.success("متن کپی شد!", {
+          autoClose: 1000,
+          position: "top-left",
+        });
+
+        setTimeout(() => setCopiedMessageId(null), 4000);
       })
       .catch((err) => {
         console.error("Failed to copy:", err);
@@ -1075,7 +1081,7 @@ const Chat = ({ item }) => {
                         />
                         <button
                           onClick={() => handleCopyAnswer(item.answer, index)}
-                          className="mt-4 flex items-center justify-center w-7 bg-neutral-100 h-7 rounded-lg hover:bg-neutral-200"
+                          className="mt-4 flex items-center justify-center w-7 dark:bg-transparent text-neutral-50 bg-neutral-100 h-7 rounded-lg hover:bg-neutral-200"
                           style={{
                             color:
                               copiedMessageId === index ? "#3dc909" : "#444",
@@ -1099,17 +1105,27 @@ const Chat = ({ item }) => {
                           ) : (
                             <svg
                               xmlns="http://www.w3.org/2000/svg"
-                              fill="none"
+                              width="16"
+                              height="16"
                               viewBox="0 0 24 24"
-                              strokeWidth={1.5}
+                              fill="none"
                               stroke="currentColor"
-                              className="size-5"
+                              stroke-width="2"
+                              stroke-linecap="round"
+                              stroke-linejoin="round"
+                              className="dark:text-neutral-100"
                             >
-                              <path
-                                strokeLinecap="round"
-                                strokeLinejoin="round"
-                                d="M8.25 7.5V6.108c0-1.135.845-2.098 1.976-2.192.373-.03.748-.057 1.123-.08M15.75 18H18a2.25 2.25 0 0 0 2.25-2.25V6.108c0-1.135-.845-2.098-1.976-2.192a48.424 48.424 0 0 0-1.123-.08M15.75 18.75v-1.875a3.375 3.375 0 0 0-3.375-3.375h-1.5a1.125 1.125 0 0 1-1.125-1.125v-1.5A3.375 3.375 0 0 0 6.375 7.5H5.25m11.9-3.664A2.251 2.251 0 0 0 15 2.25h-1.5a2.251 2.251 0 0 0-2.15 1.586m5.8 0c.065.21.1.433.1.664v.75h-6V4.5c0-.231.035-.454.1-.664M6.75 7.5H4.875c-.621 0-1.125.504-1.125 1.125v12c0 .621.504 1.125 1.125 1.125h9.75c.621 0 1.125-.504 1.125-1.125V16.5a9 9 0 0 0-9-9Z"
-                              />
+                              <g transform="scale(-1,1) translate(-24,0)">
+                                <rect
+                                  x="9"
+                                  y="9"
+                                  width="13"
+                                  height="13"
+                                  rx="2"
+                                  ry="2"
+                                />
+                                <path d="M5 15H4a2 2 0 0 1-2-2V4a2 2 0 0 1 2-2h9a2 2 0 0 1 2 2v1" />
+                              </g>
                             </svg>
                           )}
                         </button>
@@ -1192,8 +1208,7 @@ const Chat = ({ item }) => {
             />
           </div>
           <div className="flex flex-col gap-1">
-
-            <VoiceBtn onTranscribe={setQuestion}/>
+            <VoiceBtn onTranscribe={setQuestion} />
             <button
               onClick={realtimeHandleSubmit}
               disabled={chatLoading || !question.trim()}
