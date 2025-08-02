f9dc905b3bc16291448298302ce9fa4a2aebdbd6
Mahdi Mahmoudi <dev@mahdi-mahmoudi.ir>
2025-08-02 00:59:09 -0700

    feat(VoiceAgentConversation): add display fetched voice agent instruction

--- src/pages/VoiceAgentConversation.jsx
+++ src/pages/VoiceAgentConversation.jsx
@@ -1,9 +1,30 @@
+import { useEffect, useState } from "react";
+import { voiceAgentEndpoints } from "../utils/apis";
+
 const VoiceAgentConversation = () => {
-    return (
-        <h1>
-            Voice Agent Conversation Page ✌️✌️🙌🙌
-        </h1>
-    );
-}
+  const [instruction, setInstruction] = useState(null);
+
+  useEffect(() => {
+    const fetchInstruction = async () => {
+      const res = await voiceAgentEndpoints.getVoiceAgentInstruction();
+
+      setInstruction(res.instruction);
+    };
+
+    fetchInstruction();
+  }, []);
+
+  return (
+    <div dir="ltr" className="p-5">
+      <h1>Voice Agent Conversation Page ✌️✌️🙌🙌</h1>
+
+      <h3>Instruction</h3>
+
+      <br />
+
+      <pre dir="ltr">{instruction}</pre>
+    </div>
+  );
+};
 
 export default VoiceAgentConversation;
