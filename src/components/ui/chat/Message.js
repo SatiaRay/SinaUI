44c2263e61f4a3b6f9f5922208991580fed89a6c
Mahdi Mahmoudi <dev@mahdi-mahmoudi.ir>
2025-08-28 01:32:50 -0700

    feat(chat): introduce Message component for rendering chat messages
    - Added a new Message component to encapsulate the rendering logic for both user questions and bot responses.
    - Simplified the Chat component by replacing inline message rendering with the new Message component.
    - Enhanced code maintainability and readability by separating concerns.

---
+++ src/components/ui/chat/Message.js
@@ -0,0 +1,101 @@
+import {
+  copyToClipboard,
+  formatTimestamp,
+  stripHtmlTags,
+} from "../../../utils/helpers";
+import React, { useEffect, useRef, useState } from "react";
+
+const Message = ({ messageIndex, data, copyAnswer }) => {
+  const textRef = useRef(null);
+  const [copiedMessageId, setCopiedMessageId] = useState(null);
+
+  return (
+    <>
+      {data.type === "question" ? (
+        <div className="bg-blue-100/70 md:ml-16 dark:bg-blue-900/20 p-3 rounded-lg text-right">
+          <div className="flex justify-between items-center mb-1">
+            <span className="text-xs text-gray-500 dark:text-gray-400">
+              {formatTimestamp(data.timestamp)}
+            </span>
+            <span className="text-xs font-medium text-blue-600 dark:text-blue-400">
+              شما
+            </span>
+          </div>
+          <pre
+            className="text-gray-800 pt-1 leading-5 dark:text-white [&_a]:text-blue-600 [&_a]:hover:text-blue-700 [&_a]:underline [&_a]:break-all dark:[&_a]:text-blue-400 dark:[&_a]:hover:text-blue-300"
+            dangerouslySetInnerHTML={{ __html: data.text }}
+          />
+        </div>
+      ) : (
+        <div className=" bg-white dark:bg-gray-800 px-4 py-2 flex flex-col text-wrap md:ml-1 md:mr-8 rounded-lg">
+          <div className="flex justify-between items-center mb-2">
+            <span className="text-xs text-gray-500 dark:text-gray-400">
+              {formatTimestamp(data.timestamp)}
+            </span>
+            <span className="text-xs font-medium text-green-600 dark:text-green-400">
+              چت‌بات
+            </span>
+          </div>
+          <div>
+            <h3 className="font-bold mb-2 text-gray-900 dark:text-white">
+              پاسخ:
+            </h3>
+            <pre
+              ref={textRef}
+              style={{
+                unicodeBidi: "plaintext",
+                direction: "rtl",
+              }}
+              className="text-gray-800 flex text-wrap flex-wrap px-2 pt-2 leading-5 dark:text-white [&_table]:w-full [&_table]:border-collapse [&_table]:my-4 [&_th]:bg-white [&_th]:text-black [&_th]:p-2 [&_th]:border [&_th]:border-gray-200 [&_th]:text-right dark:[&_th]:bg-white dark:[&_th]:text-black dark:[&_th]:border-gray-700 [&_td]:p-2 [&_td]:border [&_td]:border-gray-200 [&_td]:text-right dark:[&_td]:text-white dark:[&_td]:border-gray-700 [&_a]:text-blue-600 [&_a]:hover:text-blue-700 [&_a]:underline [&_a]:break-all dark:[&_a]:text-blue-400 dark:[&_a]:hover:text-blue-300"
+              dangerouslySetInnerHTML={{ __html: data.answer }}
+            />
+            <button
+              onClick={() => copyAnswer(data.answer, messageIndex)}
+              className="mt-4 flex items-center justify-center w-7 h-7 rounded-lg hover:bg-gray-100 dark:hover:bg-gray-700"
+              style={{
+                color: copiedMessageId === messageIndex ? "#3dc909" : "#444",
+              }}
+            >
+              {copiedMessageId === messageIndex ? (
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  strokeWidth={1.5}
+                  stroke="currentColor"
+                  className="size-4"
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    d="m4.5 12.75 6 6 9-13.5"
+                  />
+                </svg>
+              ) : (
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="16"
+                  height="16"
+                  viewBox="0 0 24 24"
+                  fill="none"
+                  stroke="currentColor"
+                  strokeWidth="2"
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  className="dark:text-gray-100"
+                >
+                  <g transform="scale(-1,1) translate(-24,0)">
+                    <rect x="9" y="9" width="13" height="13" rx="2" ry="2" />
+                    <path d="M5 15H4a2 2 0 0 1-2-2V4a2 2 0 0 1 2-2h9a2 2 0 0 1 2 2v1" />
+                  </g>
+                </svg>
+              )}
+            </button>
+          </div>
+        </div>
+      )}
+    </>
+  );
+};
+
+export default Message;
