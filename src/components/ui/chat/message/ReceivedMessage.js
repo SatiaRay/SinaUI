c000b52933ae1abf0844ea4591196b5e27f715a0
AmirPhpDeveloper <amiralimahmoodi8913@gmail.com>
2025-09-08 11:40:17 +0330

    feat(chatUI): rewrite ReceivedMessage like GPT✨

--- src/components/ui/chat/message/ReceivedMessage.js
+++ src/components/ui/chat/message/ReceivedMessage.js
@@ -2,7 +2,7 @@ import { formatTimestamp } from "../../../../utils/helpers";
 
 const ReceivedMessage = ({ children, timestamp }) => {
   return (
-    <div className="bg-white dark:bg-gray-800 px-4 py-2 flex flex-col text-wrap md:ml-1 md:mr-8 rounded-lg min-w-[10rem] max-w-3xl justify-self-end">
+    <div className="bg-gray-50 dark:bg-gray-900 px-4 py-2 flex flex-col text-wrap md:ml-1 md:mr-8 rounded-lg min-w-[10rem] w-full max-w-full justify-self-end">
       <div className="flex justify-between items-center mb-2">
         <span className="text-xs text-gray-500 dark:text-gray-400">
           {formatTimestamp(timestamp)}
