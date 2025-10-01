3bbe69909f9672040452a70258ef33fcfa9b27e2
mahya-jafari <mahyajfri37@gmail.com>
2025-10-01 11:47:18 +0330

    bug of previous messages

--- src/components/ui/chat/message/SentMessage.js
+++ src/components/ui/chat/message/SentMessage.js
@@ -1,13 +1,13 @@
 import { formatTimestamp } from '../../../../utils/helpers';
 
-const SentMessage = ({ children, timestamp }) => {
+const SentMessage = ({ children, created_at }) => {
   return (
-    <div className="bg-blue-100/70 md:ml-16 dark:bg-blue-900/20 p-3 rounded-lg text-right max-w-3xl min-w-[10rem] inline-block justify-self-start">
-      <div className="flex justify-between items-center mb-1">
-        <span className="text-xs text-gray-500 dark:text-gray-400">
-          {formatTimestamp(timestamp)}
+    <div className='bg-blue-100/70 md:ml-16 dark:bg-blue-900/20 p-3 rounded-lg text-right max-w-3xl min-w-[10rem] inline-block justify-self-start'>
+      <div className='flex justify-between items-center mb-1'>
+        <span className='text-xs text-gray-500 dark:text-gray-400'>
+          {formatTimestamp(created_at)}
         </span>
-        <span className="text-xs font-medium text-blue-600 dark:text-blue-400">
+        <span className='text-xs font-medium text-blue-600 dark:text-blue-400'>
           شما
         </span>
       </div>
