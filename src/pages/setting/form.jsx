c87e896d1860e8097fbb0a4d6dd6081b133a2e2c
Mahdi Mahmoudi <dev@mahdi-mahmoudi.ir>
2025-10-05 00:28:03 -0700

    feat(settings): add array-type support in settings form

--- src/pages/setting/form.jsx
+++ src/pages/setting/form.jsx
@@ -1,8 +1,18 @@
 import React, { useState, useEffect } from 'react';
 import CustomDropdown from '../../ui/dropdown';
+import Tagify from '@yaireo/tagify';
 
 const SettingsForm = ({ schema, initialValues = {}, onSubmit, isLoading }) => {
   const [formData, setFormData] = useState({});
+
+  useEffect(() => {
+    var inputElem = document.querySelector('input.tagify'); // the 'input' element which will be transformed into a Tagify component
+    var tagify = new Tagify(inputElem, {
+      // A list of possible tags. This setting is optional if you want to allow
+      // any possible tag to be added without suggesting any to the user.
+      whitelist: ['foo', 'bar', 'and baz', 0, 1, 2],
+    });
+  }, []);
 
   useEffect(() => {
     const defaults = {};
@@ -74,6 +84,24 @@ const SettingsForm = ({ schema, initialValues = {}, onSubmit, isLoading }) => {
           />
         );
 
+      case 'array':
+        return (
+          <input
+            className="tagify border rounded w-full dark:bg-gray-700 dark:border-gray-600 dark:text-white"
+            onChange={(e) => {
+              try{
+                const tags = JSON.parse(e.target.value).map((item) => item.value)
+
+                handleChange(key, tags)
+              } catch {
+                handleChange(key, [])
+              }
+            }}
+            value={value}
+            placeholder={`عدد ${label} را وارد کنید`}
+          />
+        );
+
       default:
         return (
           <input
@@ -123,3 +151,4 @@ const SettingsForm = ({ schema, initialValues = {}, onSubmit, isLoading }) => {
 };
 
 export default SettingsForm;
+
