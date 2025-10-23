88c32fa6f3c56ea09b8e2484939bad0b89a9c98e
Mahdi Mahmoudi <dev@mahdi-mahmoudi.ir>
2025-10-23 03:20:49 -0700

    feat(DocumentIndex): implement pagination in DocumentIndex component for improved document navigation

--- src/components/Chat/Document/DocumentIndex.jsx
+++ src/components/Chat/Document/DocumentIndex.jsx
@@ -6,28 +6,45 @@ import SearchDocument from './searchDocument/SearchDocument'; // Import the sepa
 import { knowledgeApi } from '../../../store/api/knowledgeApi';
 import 'react-loading-skeleton/dist/skeleton.css';
 import { SkeletonLoading } from '../../../ui/loading/skeletonLoading';
+import { Pagination } from '../../ui/pagination';
 import { useBreakpoint, useDisplay } from '../../../hooks/display';
 
 const DocumentIndex = () => {
-  const { data, isLoading, isSuccess, isError, error } =
-    knowledgeApi.useGetAllQuery();
-
   const { height, isDesktop, isLargeDisplay } = useDisplay();
 
-  if(isLoading)
+  const [page, setPage] = useState(1)
+  const perpage = 20
+
+  const { data, isLoading, isSuccess, isError, error } =
+    knowledgeApi.useGetAllQuery({ page, perpage});
+
+  if (isLoading)
     return (
       <div className="text-center">
-        <SkeletonLoading rows={(height / 150)} cols={ !isDesktop ? 1 : (isLargeDisplay ? 3: 2)} height={110} />
+        <SkeletonLoading
+          rows={height / 150}
+          cols={!isDesktop ? 1 : isLargeDisplay ? 3 : 2}
+          height={110}
+        />
       </div>
     );
 
   return (
-    <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-3'>
-      {data.documents.map((document) => (
-        <DocumentCard document={document}/>
-      ))}
+    <div className="h-full flex flex-col justify-center">
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-3 flex-1 items-start">
+        {data.documents.map((document) => (
+          <DocumentCard document={document} />
+        ))}
+      </div>
+      <Pagination
+        page={page}
+        perpage={perpage}
+        totalPages={data.pages}
+        totalItems={data.total}
+        handlePageChange={setPage}
+      />
     </div>
-  )
+  );
 };
 
 DocumentIndex.propTypes = {};
