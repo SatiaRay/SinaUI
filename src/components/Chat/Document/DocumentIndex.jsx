a6cbf4380c606b142c5ec63729e37dc483920fc9
Mahdi Mahmoudi <dev@mahdi-mahmoudi.ir>
2025-10-25 07:00:28 -0700

    fix(DocumentIndex): add key prop to DocumentCard for unique identification in list rendering

--- src/components/Chat/Document/DocumentIndex.jsx
+++ src/components/Chat/Document/DocumentIndex.jsx
@@ -33,7 +33,7 @@ const DocumentIndex = () => {
     <div className="h-full flex flex-col justify-center">
       <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-3 flex-1 items-start">
         {data.documents.map((document) => (
-          <DocumentCard document={document} />
+          <DocumentCard document={document} key={document.id}/>
         ))}
       </div>
       <Pagination
