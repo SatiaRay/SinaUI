c723a75865d60eb463c7c537cd9373f580cc89c5
Mahdi Mahmoudi <dev@mahdi-mahmoudi.ir>
2025-10-26 04:07:32 -0700

    feat(DocumentIndex): implement document deletion functionality with confirmation and error handling

--- src/components/Chat/Document/DocumentIndex.jsx
+++ src/components/Chat/Document/DocumentIndex.jsx
@@ -1,23 +1,74 @@
 // DocumentIndex.js
 import React, { useEffect, useState, useCallback } from 'react';
 import DocumentCard from './DocumentCard';
-import { Sppiner } from '../../ui/sppiner';
 import SearchDocument from './searchDocument/SearchDocument'; // Import the separate search component
-import knowledgeApi from '../../../store/api/knowledgeApi';
+import knowledgeApi, {
+  useDeleteDocumentMutation,
+} from '../../../store/api/knowledgeApi';
 import 'react-loading-skeleton/dist/skeleton.css';
 import { SkeletonLoading } from '../../../ui/loading/skeletonLoading';
 import { Pagination } from '../../ui/pagination';
-import { useBreakpoint, useDisplay } from '../../../hooks/display';
+import { useDisplay } from '../../../hooks/display';
+import { notify } from '../../../ui/toast';
+import { confirm } from '../../ui/alert/confirmation';
 
 const DocumentIndex = () => {
+  /**
+   * Response props
+   */
   const { height, isDesktop, isLargeDisplay } = useDisplay();
 
-  const [page, setPage] = useState(1)
-  const perpage = 20
+  /**
+   * Pagination props
+   */
+  const [page, setPage] = useState(1);
+  const perpage = 20;
 
+  /**
+   * List of documents
+   */
+  const [documents, setDocuments] = useState(null);
+
+  /**
+   * Fetch documents list api hook
+   */
   const { data, isLoading, isSuccess, isError, error } =
-    knowledgeApi.useGetAllDocumentsQuery({ page, perpage});
+    knowledgeApi.useGetAllDocumentsQuery({ page, perpage });
 
+  /**
+   * Store documents from request response data for optimistic mutation
+   */
+  if (isSuccess && !documents) setDocuments(data.documents);
+
+  /**
+   * Delete document api hook
+   */
+  const [deleteDocument, result] = useDeleteDocumentMutation();
+
+  /**
+   * Delete document handler
+   * @param {number} id
+   */
+  const handleDeleteDocument = async (id) => {
+    confirm({
+      title: 'حذف سند',
+      text: 'آیا از حذف این سند مطمئن هستید ؟',
+      onConfirm: async () => {
+        setDocuments(documents.filter((doc) => doc.id != id));
+
+        try {
+          await deleteDocument(id).unwrap();
+        } catch (err) {
+          notify.error('خطا در حذف سند!');
+          setDocuments(data.documents);
+        }
+      },
+    });
+  };
+
+  /**
+   * Show skeleton loading
+   */
   if (isLoading)
     return (
       <div className="text-center">
@@ -29,11 +80,28 @@ const DocumentIndex = () => {
       </div>
     );
 
+  /**
+   * Display error message when fetching fails
+   */
+  if (isError) return <p>مشکلی پیش آمده است 🛑</p>;
+
+  /**
+   * Prevent map documents when it is null
+   */
+  if (!documents) return null;
+
+  /**
+   * Display document cards list
+   */
   return (
     <div className="h-full flex flex-col justify-center">
       <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-3 flex-1 items-start">
-        {data.documents.map((document) => (
-          <DocumentCard document={document} key={document.id}/>
+        {documents.map((document) => (
+          <DocumentCard
+            document={document}
+            handleDelete={handleDeleteDocument}
+            key={document.id}
+          />
         ))}
       </div>
       <Pagination
@@ -46,7 +114,5 @@ const DocumentIndex = () => {
     </div>
   );
 };
-
-DocumentIndex.propTypes = {};
 
 export default DocumentIndex;
