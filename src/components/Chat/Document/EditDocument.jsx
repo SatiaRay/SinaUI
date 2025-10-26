ad54bbde14b6d15a3d84081b83170fa2dadd0244
Mahdi Mahmoudi <dev@mahdi-mahmoudi.ir>
2025-10-26 07:01:37 -0700

    refactor(EditDocument): integrate CKEditor configuration and enhance Tagify initialization

--- src/components/Chat/Document/EditDocument.jsx
+++ src/components/Chat/Document/EditDocument.jsx
@@ -141,71 +141,7 @@ const EditDocument = () => {
                   const value = editor.getData();
                   setDocument({...document, text: value})
                 }}
-                config={{
-                  language: 'fa',
-                  direction: 'rtl',
-                  toolbar: {
-                    items: [
-                      'heading',
-                      '|',
-                      'bold',
-                      'italic',
-                      'link',
-                      'bulletedList',
-                      'numberedList',
-                      '|',
-                      'outdent',
-                      'indent',
-                      '|',
-                      'insertTable',
-                      'undo',
-                      'redo',
-                    ],
-                  },
-                  table: {
-                    contentToolbar: [
-                      'tableColumn',
-                      'tableRow',
-                      'mergeTableCells',
-                      'tableProperties',
-                      'tableCellProperties',
-                    ],
-                    defaultProperties: {
-                      borderWidth: '1px',
-                      borderColor: '#ccc',
-                      borderStyle: 'solid',
-                      alignment: 'right',
-                    },
-                  },
-                  htmlSupport: {
-                    allow: [
-                      {
-                        name: 'table',
-                        attributes: true,
-                        classes: true,
-                        styles: true,
-                      },
-                      {
-                        name: 'tr',
-                        attributes: true,
-                        classes: true,
-                        styles: true,
-                      },
-                      {
-                        name: 'td',
-                        attributes: true,
-                        classes: true,
-                        styles: true,
-                      },
-                      {
-                        name: 'th',
-                        attributes: true,
-                        classes: true,
-                        styles: true,
-                      },
-                    ],
-                  },
-                }}
+                config={ckEditorConfig}
                 style={{
                   direction: 'rtl',
                   textAlign: 'right',
