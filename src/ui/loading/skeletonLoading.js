68a23dbfdf4b020266b434d0afc2d700b3987566
Mahdi Mahmoudi <dev@mahdi-mahmoudi.ir>
2025-10-27 06:45:14 -0700

    refactor(SkeletonLoading): enhance SkeletonLoading component by adding customizable props for base and highlight colors

--- src/ui/loading/skeletonLoading.js
+++ src/ui/loading/skeletonLoading.js
@@ -1,16 +1,21 @@
 import Skeleton, { SkeletonTheme } from 'react-loading-skeleton';
 
+export const SkeletonLoading = ({ rows = 1, cols = 1, height = 20, baseColor ="#132440", highlightColor ="#222", containerClassName="flex flex-row gap-2 my-3" }) => {
+  let skeletonRows = [];
 
-export const SkeletonLoading = ({rows= 1, cols=1, height=20}) => {
-
-    let skeletonRows = []
-
-    for (let i = 0; i < rows; i++) {
-        skeletonRows.push(<Skeleton key={i} count={cols} containerClassName='flex flex-row gap-2 my-3' height={height}/>)        
-    }
+  for (let i = 0; i < rows; i++) {
+    skeletonRows.push(
+      <Skeleton
+        key={i}
+        count={cols}
+        containerClassName={containerClassName}
+        height={height}
+      />
+    );
+  }
 
   return (
-    <SkeletonTheme baseColor="#132440" highlightColor="#222">
+    <SkeletonTheme baseColor={baseColor} highlightColor={highlightColor}>
       {skeletonRows}
     </SkeletonTheme>
   );
