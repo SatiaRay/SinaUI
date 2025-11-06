872a7d51e90ccc315ad5bfbf93612bf964e0587d
AmirPhpDeveloper <amiralimahmoodi8913@gmail.com>
2025-11-06 09:11:03 +0330

    chore: delete old files

--- src/pages/Monitorings/index.jsx
+++
@@ -1,4 +0,0 @@
-export { default as MonitoringIndex } from './MonitoringIndex/MonitoringIndex';
-export { default as RecentLogsPage } from './RecentLogsPage/RecentLogsPage';
-export { default as LogSearchPage } from './LogSearchPage/LogSearchPage';
-export { default as ToolUsageStats } from './ToolUsageStats/ToolUsageStats';
--- src/pages/document/index.jsx
+++
@@ -1,3 +0,0 @@
-export { default as DocumentIndex } from './DocumentIndex/DocumentIndexPage';
-export { default as CreateDocument } from './CreateDocument/CreateDocumentPage';
-export { default as EditDocument } from './EditDocument/EditDocumentPage';
