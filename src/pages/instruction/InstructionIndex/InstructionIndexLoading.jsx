33b8b552e2522ec3df27f8fa537c08d282aeb0a1
Mahdi Mahmoudi <dev@mahdi-mahmoudi.ir>
2025-11-14 23:15:42 -0800

    refactor: simplify InstructionIndexLoading component by removing unnecessary skeleton elements and integrating SkeletonLoading for enhanced loading display

--- src/pages/instruction/InstructionIndex/InstructionIndexLoading.jsx
+++ src/pages/instruction/InstructionIndex/InstructionIndexLoading.jsx
@@ -1,107 +1,35 @@
+// InstructionIndexLoading.js
+import { SkeletonLoading } from '../../../ui/loading/skeletonLoading';
+import { useDisplay } from '../../../hooks/display';
 import Skeleton from 'react-loading-skeleton';
 import 'react-loading-skeleton/dist/skeleton.css';
 
 export const InstructionIndexLoading = () => {
+  /**
+   * Response props
+   */
+  const { height, isLargeDisplay } = useDisplay();
+
   return (
-    <div className="p-4 md:pt-12 container mx-auto overflow-x-hidden">
-      <div className="flex max-md:flex-col md:gap-0 gap-2 pt-6 md:pt-0 justify-between items-center mb-4">
+    <div className="text-center container mx-auto mt-3 md:mt-0 px-3 md:px-0">
+      <div className='flex justify-between md:pl-2 mb-6 items-center'>
+        <h3 className="text-3xl">دستورالعمل‌ها</h3>
         <Skeleton
           baseColor="#374151"
           highlightColor="#5c626b"
-          height={28}
-          width={220}
+          height={45}
           className="inline"
+          width={110}
         />
-        <div className="max-md:w-full flex justify-between items-center gap-2 w-1/2">
-          <Skeleton
-            baseColor="#374151"
-            highlightColor="#5c626b"
-            height={36}
-            className="flex-1"
-          />
-          <div className="max-md:w-full flex items-center w-1/2 justify-end space-x-2 rtl:space-x-reverse">
-            <Skeleton
-              baseColor="#374151"
-              highlightColor="#5c626b"
-              height={36}
-              width={140}
-            />
-            <Skeleton
-              baseColor="#374151"
-              highlightColor="#5c626b"
-              height={36}
-              width={140}
-            />
-          </div>
-        </div>
       </div>
-      <div className="overflow-x-auto rounded-lg shadow-md">
-        <div className="inline-block min-w-full align-middle">
-          <div className="overflow-hidden border-b border-gray-200 dark:border-gray-700 rounded-lg">
-            <div className="bg-neutral-200 dark:bg-gray-700 p-3">
-              <Skeleton
-                baseColor="#374151"
-                highlightColor="#5c626b"
-                height={18}
-                className="w-full"
-              />
-            </div>
-            <div className="bg-white dark:bg-gray-800 divide-y divide-gray-200 dark:divide-gray-700">
-              {[...Array(7)].map((_, i) => (
-                <div
-                  key={i}
-                  className="grid [grid-template-columns:250px_4.3fr_0.5fr_0.5fr_0.5fr] gap-3 p-3"
-                >
-                  <Skeleton
-                    baseColor="#374151"
-                    highlightColor="#5c626b"
-                    height={60}
-                  />
-                  <Skeleton
-                    baseColor="#374151"
-                    highlightColor="#5c626b"
-                    height={60}
-                  />
-                  <Skeleton
-                    baseColor="#374151"
-                    highlightColor="#5c626b"
-                    height={60}
-                  />
-                  <Skeleton
-                    baseColor="#374151"
-                    highlightColor="#5c626b"
-                    height={60}
-                  />
-                  <Skeleton
-                    baseColor="#374151"
-                    highlightColor="#5c626b"
-                    height={60}
-                  />
-                </div>
-              ))}
-            </div>
-          </div>
-        </div>
-      </div>
-      <div className="bg-white shadow-lg border-t dark:bg-gray-800 p-3 flex items-center justify-between mt-4 rounded-lg">
-        <Skeleton
-          baseColor="#374151"
-          highlightColor="#5c626b"
-          height={18}
-          width={160}
-        />
-        <div className="flex items-center">
-          {[...Array(3)].map((_, i) => (
-            <Skeleton
-              key={i}
-              baseColor="#374151"
-              highlightColor="#5c626b"
-              height={40}
-              width={32}
-            />
-          ))}
-        </div>
-      </div>
+
+      <SkeletonLoading
+        rows={Math.floor(height / 150) - 1}
+        cols={!isLargeDisplay ? 1 : 3}
+        height={150}
+        containerClassName='flex flex-row my-3'
+        className='md:mx-2'
+      />
     </div>
   );
 };
