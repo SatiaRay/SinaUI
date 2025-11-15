62b4704cc4cb6fc183fe9406b9bafda37d15864e
Mahdi Mahmoudi <dev@mahdi-mahmoudi.ir>
2025-11-14 23:05:36 -0800

    refactor: update EditInstructionLoading component layout and skeleton dimensions for improved loading experience

--- src/pages/instruction/EditInstruction/EditInstructionLoading.jsx
+++ src/pages/instruction/EditInstruction/EditInstructionLoading.jsx
@@ -1,92 +1,80 @@
+// EditInstructionLoading.js
 import Skeleton from 'react-loading-skeleton';
 import 'react-loading-skeleton/dist/skeleton.css';
 
 export const EditInstructionLoading = () => {
   return (
-    <div className="w-full max-w-[680px] mx-auto">
-      <div className="grid grid-cols-1">
+    <div className="bg-white dark:bg-gray-800 rounded-lg shadow flex flex-col h-full overflow-hidden w-full p-3 md:p-8 pt-7">
+      <div className="grid grid-cols-1 md:grid-cols-2 justify-between">
         <Skeleton
           baseColor="#374151"
           highlightColor="#5c626b"
           height={30}
-          width={180}
           className="inline mt-4"
+          width={150}
         />
-      </div>
-      <div className="grid gap-1 mt-5">
-        <Skeleton
-          baseColor="#374151"
-          highlightColor="#5c626b"
-          height={18}
-          width={100}
-        />
-        <Skeleton
-          baseColor="#374151"
-          highlightColor="#5c626b"
-          height={40}
-          className="w-full"
-        />
-      </div>
-      <div className="grid gap-1 mt-5">
-        <Skeleton
-          baseColor="#374151"
-          highlightColor="#5c626b"
-          height={18}
-          width={100}
-        />
-        <Skeleton
-          baseColor="#374151"
-          highlightColor="#5c626b"
-          height={100}
-          className="w-full"
-        />
-      </div>
-      <div className="grid grid-cols-1 md:grid-cols-2 gap-4 mt-5">
-        <div className="grid gap-1">
+        <div className="flex gap-3 justify-end mt-3 w-full">
           <Skeleton
             baseColor="#374151"
             highlightColor="#5c626b"
-            height={18}
-            width={70}
-            className="ml-auto"
+            height={45}
+            containerClassName="w-1/2 md:w-[80px]"
+            className="inline"
           />
           <Skeleton
             baseColor="#374151"
             highlightColor="#5c626b"
-            height={40}
+            height={45}
+            containerClassName="w-1/2 md:w-[110px]"
+            className="inline"
+          />
+        </div>
+      </div>
+
+      <div className='grid grid-cols-1 lg:grid-cols-2 gap-3'>
+        <div className="grid gap-1 mt-5 md:mt-5">
+          <Skeleton
+            baseColor="#374151"
+            highlightColor="#5c626b"
+            height={30}
+            width={80}
+          />
+          <Skeleton
+            baseColor="#374151"
+            highlightColor="#5c626b"
+            height={45}
             className="w-full"
           />
         </div>
-        <div className="grid gap-1">
+
+        <div className="grid gap-1 mt-5">
           <Skeleton
             baseColor="#374151"
             highlightColor="#5c626b"
-            height={18}
-            width={60}
-            className="ml-auto"
+            height={30}
+            width={80}
           />
           <Skeleton
             baseColor="#374151"
             highlightColor="#5c626b"
-            height={40}
+            height={45}
             className="w-full"
           />
         </div>
       </div>
-      <div className="flex gap-3 justify-end ltr:justify-start mt-4 w-full">
+
+      <div className="grid gap-1 mt-5">
         <Skeleton
           baseColor="#374151"
           highlightColor="#5c626b"
-          height={44}
-          containerClassName="w-[90px]"
-          className="inline"
+          height={30}
+          width={80}
         />
         <Skeleton
           baseColor="#374151"
           highlightColor="#5c626b"
-          height={44}
-          containerClassName="w-[130px]"
-          className="inline"
+          height={200}
+          className="w-full"
         />
       </div>
     </div>
