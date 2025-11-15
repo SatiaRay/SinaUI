0c22be89316364bf791aa9697cc866326ef193c9
Mahdi Mahmoudi <dev@mahdi-mahmoudi.ir>
2025-11-14 21:55:34 -0800

    fix: adjust grid layout in InstructionIndexPage to improve responsiveness by changing column configuration

--- src/pages/instruction/InstructionIndex/InstructionIndexPage.jsx
+++ src/pages/instruction/InstructionIndex/InstructionIndexPage.jsx
@@ -88,7 +88,7 @@ const InstructionIndexPage = () => {
         </Link>
       </div>
 
-      <div className="flex flex-col p-3 md:p-0 md:grid grid-cols-1 lg:grid-cols-3 gap-3">
+      <div className="flex flex-col p-3 md:p-0 md:grid grid-cols-1 lg:grid-cols-2 xl:grid-cols-3 gap-3">
         {instructions.map((instruction) => (
           <div
             key={instruction.id}
