825905e5e95a7fe68f0f0b5d5cb4d1c0a43f5b76
Mahdi Mahmoudi <dev@mahdi-mahmoudi.ir>
2025-11-14 21:54:20 -0800

    refactor: rename create instruction functions and update UI elements for better clarity and consistency

--- src/pages/instruction/CreateInstruction/CreateInstructionPage.jsx
+++ src/pages/instruction/CreateInstruction/CreateInstructionPage.jsx
@@ -1,9 +1,8 @@
+// CreateInstructionPage.js
 import React, { useState, useEffect } from 'react';
 import { Link, useNavigate } from 'react-router-dom';
-import CustomDropdown from '../../../ui/dropdown';
 import { notify } from '../../../ui/toast';
 import { Sppiner } from '../../../components/ui/sppiner';
-import Error from '../InstructionError';
 import { useCreateInstructionMutation } from 'store/api/ai-features/instructionApi';
 
 const CreateInstructionPage = () => {
@@ -23,9 +22,9 @@ const CreateInstructionPage = () => {
   });
 
   /**
-   * Create instruction request hook
+   * Store instruction request hook
    */
-  const [createInstruction, { isLoading, isSuccess, isError, error, reset }] =
+  const [storeInstruction, { isLoading, isSuccess, isError, error }] =
     useCreateInstructionMutation();
 
   /**
@@ -33,38 +32,17 @@ const CreateInstructionPage = () => {
    */
   useEffect(() => {
     if (isSuccess) {
-      notify.success('دستورالعمل با موفقیت ایجاد شد !');
-      navigate('/instructions');
+      notify.success('دستورالعمل با موفقیت اضافه شد !');
+      navigate('/instruction');
     }
-  }, [isSuccess, navigate]);
+  }, [isSuccess]);
 
   /**
-   * Create instruction handler
+   * Store instruction handler
    */
-  const handleCreateInstruction = () => {
-    createInstruction(instruction);
+  const handleStoreInstruction = () => {
+    storeInstruction(instruction);
   };
-
-  /**
-   * Form field change handler
-   */
-  const handleChange = (name, value) => {
-    setInstruction((prev) => ({
-      ...prev,
-      [name]: name === 'status' ? Number(value) : value,
-    }));
-  };
-
-  const agentTypeOptions = [
-    { value: 'both', label: 'همه' },
-    { value: 'text_agent', label: 'ربات متنی' },
-    { value: 'voice_agent', label: 'ربات صوتی' },
-  ];
-
-  const statusOptions = [
-    { value: 1, label: 'فعال' },
-    { value: 0, label: 'غیرفعال' },
-  ];
 
   return (
     <div className="bg-white dark:bg-gray-800 rounded-lg shadow flex flex-col h-full overflow-hidden w-full">
@@ -72,49 +50,65 @@ const CreateInstructionPage = () => {
         <div className="flex justify-between md:items-center mb-4 max-md:flex-col max-md:gap-2">
           <div className="flex items-center gap-4">
             <h2 className="text-xl font-semibold text-gray-800 dark:text-gray-200 border-r-2 border-blue-500 pr-2 max-md:my-2">
-              ایجاد دستورالعمل جدید
+              افزودن دستورالعمل جدید
             </h2>
           </div>
-
           <div className="flex gap-2 max-md:justify-between">
             <button
-              onClick={handleCreateInstruction}
-              disabled={isLoading}
-              className="px-4 py-2 flex items-center justify-center max-md:w-1/2 bg-green-500 text-white rounded-lg hover:bg-green-600 disabled:opacity-50 gap-2"
+              onClick={handleStoreInstruction}
+              disabled={false}
+              className="px-4 py-2 flex items-center justify-center max-md:w-1/2 bg-green-500 text-white rounded-lg hover:bg-green-600 disabled:opacity-50 flex items-center gap-2"
             >
               {isLoading ? <Sppiner size={8} /> : 'ذخیره'}
             </button>
-
             <Link
-              to="/instructions"
+              to={'/instruction'}
               className="px-6 py-3 max-md:w-1/2 flex items-center justify-center rounded-lg font-medium transition-all bg-gray-300 dark:bg-gray-700 dark:hover:bg-gray-600"
             >
               بازگشت
             </Link>
           </div>
         </div>
-        {isError && (
-          <div className="mb-4">
-            <Error
-              message={error?.data?.message}
-              defaultMessage="خطایی در ایجاد دستورالعمل رخ داده است."
-              reset={() => reset()}
-            />
-          </div>
-        )}
+
         <div className="flex-1 flex flex-col min-h-0">
-          <div className="my-2 md:my-3">
-            <h3 className="text-lg font-medium text-gray-900 dark:text-white mb-2">
-              برچسب:
-            </h3>
-            <input
-              type="text"
-              value={instruction.label}
-              onChange={(e) => handleChange('label', e.target.value)}
-              className="w-full px-3 py-2 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:text-white"
-              placeholder="برچسب دستورالعمل"
-            />
+          <div className='grid grid-cols-1 lg:grid-cols-2 gap-3'>
+            <div>
+              <h3 className="text-lg font-medium text-gray-900 dark:text-white mb-2">
+                برچسب:
+              </h3>
+              <input
+                type="text"
+                value={instruction.label}
+                onChange={(e) =>
+                  setInstruction({ ...instruction, label: e.target.value })
+                }
+                className="w-full px-3 pt-2 pb-3 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:text-white"
+                placeholder="برچسب دستورالعمل"
+              />
+            </div>
+
+            <div>
+              <div className="w-full">
+                <h3 className="text-lg font-medium text-gray-900 dark:text-white mb-2">
+                  وضعیت:
+                </h3>
+                <select
+                  value={instruction.status}
+                  onChange={(e) =>
+                    setInstruction({
+                      ...instruction,
+                      status: Number(e.target.value),
+                    })
+                  }
+                  className="w-full px-3 py-2 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:text-white"
+                >
+                  <option value={1}>فعال</option>
+                  <option value={0}>غیرفعال</option>
+                </select>
+              </div>
+            </div>
           </div>
+
           <div className="my-2 md:my-3">
             <h3 className="text-lg font-medium text-gray-900 dark:text-white mb-2">
               متن:
@@ -122,38 +116,12 @@ const CreateInstructionPage = () => {
             <textarea
               rows={6}
               value={instruction.text}
-              onChange={(e) => handleChange('text', e.target.value)}
+              onChange={(e) =>
+                setInstruction({ ...instruction, text: e.target.value })
+              }
               className="w-full px-3 py-2 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:text-white"
               placeholder="متن دستورالعمل..."
             />
-          </div>
-          <div className="my-2 md:my-3 grid grid-cols-1 md:grid-cols-2 gap-3">
-            <div className="w-full">
-              <h3 className="text-lg font-medium text-gray-900 dark:text-white mb-2">
-                نوع ربات:
-              </h3>
-              <CustomDropdown
-                options={agentTypeOptions}
-                value={instruction.agent_type}
-                onChange={(value) => handleChange('agent_type', value)}
-                placeholder="نوع ربات را انتخاب کنید"
-                className="w-full"
-                parentStyle="w-full"
-              />
-            </div>
-            <div className="w-full">
-              <h3 className="text-lg font-medium text-gray-900 dark:text-white mb-2">
-                وضعیت:
-              </h3>
-              <CustomDropdown
-                options={statusOptions}
-                value={instruction.status}
-                onChange={(value) => handleChange('status', value)}
-                placeholder="وضعیت را انتخاب کنید"
-                className="w-full"
-                parentStyle="w-full"
-              />
-            </div>
           </div>
         </div>
       </div>
