7bdf9c5b5e1ecf267336c1a609768adc99bbe539
AmirPhpDeveloper <amiralimahmoodi8913@gmail.com>
2025-11-16 14:00:03 +0330

    fix: skeleton colors
    - add glbal colors for skeletons

--- src/pages/document/DocumentIndex/DocumentIndexLoading.jsx
+++ src/pages/document/DocumentIndex/DocumentIndexLoading.jsx
@@ -10,12 +10,12 @@ export const DocumentIndexLoading = () => {
 
   return (
     <div className="text-center container mx-auto mt-3 md:mt-0 px-3 md:px-0">
-      <div className='flex justify-between md:pl-2 mb-6 items-center'>
+      <div className="flex justify-between md:pl-2 mb-6 items-center">
         <h3 className="text-3xl">مستندات</h3>
 
         <Skeleton
-          baseColor="#374151"
-          highlightColor="#5c626b"
+          baseColor="var(--skeleton-base-color)"
+          highlightColor="var(--skeleton-highlight-color)"
           height={45}
           className="inline"
           width={110}
@@ -25,10 +25,9 @@ export const DocumentIndexLoading = () => {
         rows={height / 150}
         cols={!isLargeDisplay ? 1 : 3}
         height={110}
-        containerClassName='flex flex-row my-3'
-        className='md:mx-2'
+        containerClassName="flex flex-row my-3"
+        className="md:mx-2"
       />
     </div>
   );
 };
-
