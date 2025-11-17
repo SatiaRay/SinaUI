eaca64a4c8f25d932744acc3c8ce3b389994e276
Mahdi Mahmoudi <dev@mahdi-mahmoudi.ir>
2025-11-16 22:54:11 -0800

    feat(workflow): enhance WorkflowEditorSidebar with extendable menu and improved layout

--- src/components/workflow/WorkflowEditorSidebar.js
+++ src/components/workflow/WorkflowEditorSidebar.js
@@ -1,7 +1,4 @@
 import React, { useEffect, useState } from 'react';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faChevronUp, faChevronDown } from '@fortawesome/free-solid-svg-icons';
-import CustomDropdown from '../../ui/dropdown';
 import {
   PlayIcon,
   PlusCircleIcon,
@@ -10,23 +7,41 @@ import {
   XCircleIcon,
   CheckIcon,
 } from '@heroicons/react/24/outline';
+import { PanelLeft, PanelLeftClose } from 'lucide-react';
+import { useDisplay } from 'hooks/display';
 
-const WorkflowEditorSidebar = ({
-  addNode,
-  setShowChatModal,
-}) => {
+const WorkflowEditorSidebar = ({ addNode, setShowChatModal }) => {
   const [isMenuOpen, setIsMenuOpen] = useState(true);
   const toggleMenu = () => {
     setIsMenuOpen(!isMenuOpen);
   };
 
+  /**
+   * Sidebar extendable state
+   */
+  const [extended, setExtended] = useState(false);
+
+  /**
+   * Display util hooks
+   */
+  const { isDesktop } = useDisplay();
+
   const buttonStyles =
     'flex items-center gap-2 w-full px-3 py-1.5 text-sm font-medium text-white rounded-md transition-all duration-200 hover:scale-102 hover:shadow-sm focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-white dark:focus:ring-offset-gray-900';
 
   return (
-    <div className="absolute left-0 top-0 z-10 bg-white dark:bg-gray-800 shadow-md overflow-hidden transition-all duration-300 h-full border-r border-gray-700">
-      <style>
-        {`
+    <>
+      {/* Sandbox */}
+      <div
+        className={`absolute w-full h-full top-0 left-0 dark:bg-gray-600 bg-gray-400 opacity-80 z-10 ${extended && !isDesktop ? '' : 'hidden'}`}
+      ></div>
+
+      <div
+        className="absolute left-0 top-0 z-10 bg-white dark:bg-gray-800 shadow-md overflow-x-hidden overflow-y-auto transition-all duration-300 h-full border-r border-gray-300 dark:border-gray-700 w-auto"
+        style={{ width: extended ? '250px' : 'auto' }}
+      >
+        <style>
+          {`
           :root {
             --primary-bg: #F9FAFB;
             --primary-text: #1F2937;
@@ -43,74 +58,123 @@ const WorkflowEditorSidebar = ({
             --primary-text: #F9FAFB;
           }
         `}
-      </style>
-     
-      <div
-        className={` flex flex-col gap-1.5 p-2 transition-all duration-300 ease-in-out ${
-          isMenuOpen
-            ? 'max-h-[calc(100vh-110px)] opacity-100'
-            : 'max-h-0 opacity-0 overflow-hidden'
-        }`}
-      >
-        <button
-          onClick={() => addNode('start')}
-          className={`${buttonStyles} bg-[var(--accent-blue)] hover:bg-blue-600 focus:ring-blue-400 w-[50px] h-[50px] rounded-[50%] text-center px-0`}
-          title='نقطه شروع فرایند'
+        </style>
+
+        {/* Sidebar Menu */}
+        <div
+          className={`flex flex-col justify-between items-stretch h-full gap-1.5 p-2 transition-all duration-300 ease-in-out ${
+            isMenuOpen
+              ? 'max-h-[calc(100vh-110px)] opacity-100'
+              : 'max-h-0 opacity-0 overflow-hidden'
+          }`}
         >
-          <PlusCircleIcon className="h-10 w-10 px-0 mx-auto" />
-          {/* <span>شروع</span> */}
-        </button>
-        <button
-          onClick={() => addNode('process')}
-          className={`${buttonStyles} bg-[var(--accent-blue)] hover:bg-blue-600 focus:ring-blue-400 w-[50px] h-[50px] rounded-[50%]`}
-          title='فرایند'
-        >
-          <CogIcon className="h-10 w-10 px-0 mx-auto" />
-          {/* <span>فرآیند</span> */}
-        </button>
-        <button
-          onClick={() => addNode('decision')}
-          className={`${buttonStyles} bg-[var(--accent-yellow)] hover:bg-yellow-600 focus:ring-yellow-400 w-[50px] h-[50px] rounded-[50%]`}
-          title='تصمیم'
-        >
-          <QuestionMarkCircleIcon className="h-10 w-10 px-0 mx-auto" />
-          {/* <span>تصمیم</span> */}
-        </button>
-        <button
-          onClick={() => addNode('function')}
-          className={`${buttonStyles} bg-[var(--accent-purple)] hover:bg-purple-600 focus:ring-purple-400 w-[50px] h-[50px] rounded-[50%]`}
-          title='تابع'
-        >
-          <CogIcon className="h-10 w-10 px-0 mx-auto" />
-          {/* <span>تابع</span> */}
-        </button>
-        <button
-          onClick={() => addNode('response')}
-          className={`${buttonStyles} bg-[var(--accent-orange)] hover:bg-orange-600 focus:ring-orange-400 w-[50px] h-[50px] rounded-[50%]`}
-          title='پاسخ'
-        >
-          <QuestionMarkCircleIcon className="h-10 w-10 px-0 mx-auto" />
-          {/* <span>پاسخ</span> */}
-        </button>
-        <button
-          onClick={() => addNode('end')}
-          className={`${buttonStyles} bg-[var(--accent-red)] hover:bg-red-600 focus:ring-red-400 w-[50px] h-[50px] rounded-[50%]`}
-          title='پایان'
-        >
-          <XCircleIcon className="h-10 w-10 px-0 mx-auto" />
-          {/* <span>پایان</span> */}
-        </button>
-        <hr className="border-gray-200 dark:border-gray-700 my-1" />
-        <button
-          onClick={() => setShowChatModal(true)}
-          className={`${buttonStyles} bg-[var(--accent-teal)] hover:bg-teal-600 focus:ring-teal-400 border border-teal-400 w-[50px] h-[50px] rounded-[50%]`}
-          title='اجرا'
-        >
-          <PlayIcon className="h-10 w-10 px-0 mx-auto" />
-          {/* <span>اجرا</span> */}
-        </button>
+          {/* Menu options  */}
+          <div className="grid gap-2">
+            {extended && (
+              <>
+                <h3 className={`text-lg my-0 pt-2 mr-1`}>منو</h3>
+                <hr className="border-gray-200 dark:border-gray-700 my-1" />
+              </>
+            )}
+
+            <button
+              onClick={() => addNode('start')}
+              className={`${buttonStyles} bg-[var(--accent-blue)] hover:bg-blue-600 focus:ring-blue-400 h-[45px] rounded-lg text-center`}
+              style={{ width: extended ? '100%' : '50px' }}
+              title="نقطه شروع فرایند"
+            >
+              <PlusCircleIcon
+                className="w-[30px] px-0"
+                style={{ margin: extended ? '0' : 'auto' }}
+              />
+              <span className={extended ? '' : 'hidden'}>شروع</span>
+            </button>
+            <button
+              onClick={() => addNode('process')}
+              className={`${buttonStyles} bg-[var(--accent-blue)] hover:bg-blue-600 focus:ring-blue-400 h-[45px] rounded-lg`}
+              style={{ width: extended ? '100%' : '50px' }}
+              title="فرایند"
+            >
+              <CogIcon
+                className="w-[30px] px-0"
+                style={{ margin: extended ? '0' : 'auto' }}
+              />
+              <span className={extended ? '' : 'hidden'}>فرآیند</span>
+            </button>
+            <button
+              onClick={() => addNode('decision')}
+              className={`${buttonStyles} bg-[var(--accent-yellow)] hover:bg-yellow-600 focus:ring-yellow-400 h-[45px] rounded-lg`}
+              style={{ width: extended ? '100%' : '50px' }}
+              title="تصمیم"
+            >
+              <QuestionMarkCircleIcon
+                className="w-[30px] px-0"
+                style={{ margin: extended ? '0' : 'auto' }}
+              />
+              <span className={extended ? '' : 'hidden'}>تصمیم</span>
+            </button>
+            <button
+              onClick={() => addNode('function')}
+              className={`${buttonStyles} bg-[var(--accent-purple)] hover:bg-purple-600 focus:ring-purple-400 h-[45px] rounded-lg`}
+              style={{ width: extended ? '100%' : '50px' }}
+              title="تابع"
+            >
+              <CogIcon
+                className="w-[30px] px-0"
+                style={{ margin: extended ? '0' : 'auto' }}
+              />
+              <span className={extended ? '' : 'hidden'}>تابع</span>
+            </button>
+            <button
+              onClick={() => addNode('response')}
+              className={`${buttonStyles} bg-[var(--accent-orange)] hover:bg-orange-600 focus:ring-orange-400 h-[45px] rounded-lg`}
+              style={{ width: extended ? '100%' : '50px' }}
+              title="پاسخ"
+            >
+              <QuestionMarkCircleIcon
+                className="w-[30px] px-0"
+                style={{ margin: extended ? '0' : 'auto' }}
+              />
+              <span className={extended ? '' : 'hidden'}>پاسخ</span>
+            </button>
+            <button
+              onClick={() => addNode('end')}
+              className={`${buttonStyles} bg-[var(--accent-red)] hover:bg-red-600 focus:ring-red-400 h-[45px] rounded-lg`}
+              style={{ width: extended ? '100%' : '50px' }}
+              title="پایان"
+            >
+              <XCircleIcon
+                className="w-[30px] px-0"
+                style={{ margin: extended ? '0' : 'auto' }}
+              />
+              <span className={extended ? '' : 'hidden'}>پایان</span>
+            </button>
+            <hr className="border-gray-200 dark:border-gray-700 my-1" />
+            <button
+              onClick={() => {
+                setShowChatModal(true)
+                setExtended(false)
+              }}
+              className={`${buttonStyles} bg-[var(--accent-teal)] hover:bg-teal-600 focus:ring-teal-400 border border-teal-400 h-[45px] rounded-lg`}
+              style={{ width: extended ? '100%' : '50px' }}
+              title="اجرا"
+            >
+              <PlayIcon
+                className="w-[30px] px-0"
+                style={{ margin: extended ? '0' : 'auto' }}
+              />
+              <span className={extended ? '' : 'hidden'}>اجرا</span>
+            </button>
+          </div>
+          {/* Sidebar menu footer */}
+          <div className={extended ? 'text-right' : 'text-center'}>
+            <button onClick={() => setExtended(!extended)}>
+              {extended ? <PanelLeftClose /> : <PanelLeft />}
+            </button>
+          </div>
+        </div>
       </div>
-    </div>
+    </>
   );
 };
 
