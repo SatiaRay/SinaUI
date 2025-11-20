09e4f0d5fa96f88daffda4382e5749a673f83406
Mahdi Mahmoudi <dev@mahdi-mahmoudi.ir>
2025-11-19 20:54:48 -0800

    fix(StartNode): correct viewBox attribute formatting in SVG element

--- src/components/workflow/nodes/StartNode.jsx
+++ src/components/workflow/nodes/StartNode.jsx
@@ -10,7 +10,7 @@ const StartNode = ({ data }) => {
             xmlns="http://www.w3.org/2000/svg"
             className="h-6 w-6 bg-green-500 text-white"
             fill="none"
-            viewBox="0 0 24 24"bg-green-500
+            viewBox="0 0 24 24"
             stroke="currentColor"
           >
             <path
