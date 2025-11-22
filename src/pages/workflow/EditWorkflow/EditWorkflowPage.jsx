1478f1e53271c7eef24c432cb3d532828610ddd3
Mahdi Mahmoudi <dev@mahdi-mahmoudi.ir>
2025-11-22 04:19:27 -0800

    style(EditWorkflowPage): update border radius for improved layout consistency

--- src/pages/workflow/EditWorkflow/EditWorkflowPage.jsx
+++ src/pages/workflow/EditWorkflow/EditWorkflowPage.jsx
@@ -99,7 +99,7 @@ const EditWorkflowPage = () => {
   if (isLoading || !workflow) return <EditWorkflowLoading />;
 
   return (
-    <div className="bg-white dark:bg-gray-800 rounded-lg shadow flex flex-col h-full overflow-auto w-full">
+    <div className="bg-white dark:bg-gray-800 md:rounded-lg shadow flex flex-col h-full overflow-auto w-full">
       <div className="flex-1 flex flex-col p-3 md:p-8 pt-3 md:pt-10">
         <div className="flex justify-between md:items-center mb-4 max-md:flex-col max-md:gap-2">
           <div className="flex items-center gap-4">
