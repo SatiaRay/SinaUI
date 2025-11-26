d11a688940399c05e4661b6f76ede1c757ca327f
Mahdi Mahmoudi <dev@mahdi-mahmoudi.ir>
2025-11-26 05:53:32 -0800

    feat(theme): implement theme context and toggle button for light/dark mode
    - Add ThemeContext and ThemeProvider to manage theme state and persistence
    - Introduce ThemeToggleBtn component to replace previous ThemeToggle
    - Update App.js to include ThemeProvider for theme management
    - Refactor Navbar to utilize new ThemeToggleBtn for theme switching

---
+++ src/contexts/ThemeContext.js
@@ -0,0 +1,60 @@
+import axios from 'axios';
+import { createContext, useContext, useEffect, useState } from 'react';
+import { authEndpoints, formatAxiosError } from '../utils/apis';
+import useSwr from 'swr';
+
+/**
+ * Instantiate theme context
+ */
+export const ThemeContext = createContext(null);
+
+/**
+ * Theme provider
+ * @param {jsx} children
+ * @returns 
+ */
+export const ThemeProvider = ({ children }) => {
+  /**
+   * Theme sate
+   */
+  const [theme, setTheme] = useState(() => {
+    // Get theme from localStorage or default to "light"
+    return localStorage.getItem('theme') || 'light';
+  });
+
+  /**
+   * Sync local storage with current theme for persistance theme between page reloads
+   */
+  useEffect(() => {
+    document.documentElement.classList.toggle('dark', theme === 'dark');
+
+    const editorWrappers = document.querySelectorAll('.ckeditor-wrapper');
+    editorWrappers.forEach((wrapper) => {
+      wrapper.classList.toggle('dark', theme === 'dark');
+    });
+
+    localStorage.setItem('theme', theme);
+  }, [theme]);
+
+  /**
+   * Toggle theme handler
+   */
+  const toggleTheme = () => {
+    const newTheme = theme === 'light' ? 'dark' : 'light';
+    setTheme(newTheme);
+  };
+
+  /**
+   * Return context
+   */
+  return (
+    <ThemeContext.Provider value={{ theme, setTheme, toggleTheme }}>
+      {children}
+    </ThemeContext.Provider>
+  );
+};
+
+/**
+ * @returns theme context hook
+ */
+export const useTheme = () => useContext(ThemeContext);
